9a9f97b7a844986d3ba7063d1386ba7b38aafc7e
MD RAKIB HOSEN RABBI <rakib.woosong@gmail.com>
2026-09-22 13:30:49 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x, y;
+scanf ("%d", &x);
+scanf ("%d", &y);
+printf("Sum of the two numbers:%d\n", x+y);
+printf("Differece between two numbers:%d\n", y-x);
+printf("Product of two numbers:%d\n", x*y);
+printf("Quotient of two numbers:%d\n", y/x);
+return 0;
+}
